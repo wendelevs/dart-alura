4e2435500eceb3a9bfb2b1d4f3bf8668c6181fa9
Wendel <wendelev01@gmail.com>
2024-07-31 15:40:48 -0300

    Conversoes de variaveis

--- projeto-inicial/projeto_inicial/bin/projeto_inicial.dart
+++ projeto-inicial/projeto_inicial/bin/projeto_inicial.dart
@@ -19,4 +19,29 @@ void main() {
 
   String nome = 'Wendel';
   print(nome);
+
+  /* Converter String para int */
+
+  String num1 = '5';
+  int num1_int = int.parse(num1);
+  print('String $num1 para inteiro $num1_int');
+
+  /* Converter String para double */
+
+  String num2 = '5.5';
+  double num2_double = double.parse(num2);
+  print('String $num2 para double $num2_double');
+  
+  /* Converter int para String */
+
+  int num3 = 5;
+  String num3_String = num3.toString();
+  print('int $num3 para String $num3_String');
+  
+  /* Converter double para String */
+
+  double num4 = 5.5;
+  String num4_String = num4.toString();
+  print('int $num4 para String $num4_String');
+
 }
